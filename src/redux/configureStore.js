5164df30de9e19f3e7130b25e9e1984885b41609
Sirvelasque <velasquezwalter10@yahoo.es>
2022-05-13 10:59:00 -0600

    set store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,12 @@
-import { combineReducers } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import booksReducer from './books/books';
 import categoriesReducers from './categories/categories';
 
-export default combineReducers({
+const rootReducer = combineReducers({
   booksReducer,
   categoriesReducers,
 });
+
+const store = createStore(rootReducer);
+
+export default store;
