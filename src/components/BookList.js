de116d13a869d1ade7342de1f8044a4d4b2a9fc2
Sirvelasque <velasquezwalter10@yahoo.es>
2022-05-13 19:37:54 -0600

    Link store and display books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
 
 function BookList() {
-  const books = [];
+  const books = useSelector((state) => state.books);
 
   return (
     <div>
-      {books.map((book) => (
+      {books.map((book, i) => (
         <Book
           key={uuidv4()}
-          title={book.genre}
+          index={i}
+          title={book.title}
           author={book.author}
         />
       ))}
--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import store from './redux/configureStore';
+import storeReducer from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <Provider store={storeReducer}>
+    <App />
   </Provider>,
 );
 
