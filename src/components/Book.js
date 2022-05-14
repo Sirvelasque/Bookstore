05d9f7948e2eb1261e2c73516e867b65131ffab9
Sirvelasque <velasquezwalter10@yahoo.es>
2022-05-13 20:15:11 -0600

    Dispatch categories check from react component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 function Book(props) {
-  const { title, author } = props;
+  const dispatch = useDispatch();
+  const { title, author, index } = props;
   return (
     <div className="book">
       {title}
       <hr />
       {author}
       <hr />
-      <button type="button">Delete</button>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(removeBook(index));
+        }}
+      >
+        Delete
+      </button>
     </div>
   );
 }
@@ -17,6 +27,7 @@ function Book(props) {
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default Book;
