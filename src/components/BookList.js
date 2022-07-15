e0610828a3005247646f6e406935596083222f03
Sirvelasque <velasquezwalter10@yahoo.es>
2022-07-15 16:53:56 -0600

    Optimize fetching function
    The useEffect on booklist file was executing too much times, so I added some conditions for it

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import Book from './Book';
@@ -7,11 +7,16 @@ import { getBooks } from '../redux/books/books';
 
 function BookList() {
   const books = useSelector((state) => state.books, shallowEqual);
+  const started = useRef(true);
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getBooks());
-  }, []);
+    if (started.current && books.length === 0) {
+      dispatch(getBooks());
+      started.current = false;
+    }
+  }, [started, books.length]);
+
   return (
     <div>
       <Form />
